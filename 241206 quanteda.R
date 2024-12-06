e6a0ab39f785633e12988fb40672ca93401f7fe8
Yuhe Wu <mollywu0805@pku.edu.cn>
2024-12-06 15:07:22 +0100

    241206 X-student group

---
+++ 241206 quanteda.R
@@ -0,0 +1,28 @@
+require(quanteda)
+require(quanteda.textmodels)
+require(quanteda.textstats)
+require(quanteda.textplots)
+require(readtext)
+require(devtools)
+require(quanteda.corpora)
+require(newsmap)
+require(seededlda)
+
+# basic R objects and commands
+# vec_num: numeric vector
+# vec_char: character vector
+
+# print(vec_num[1]) 输出向量中的第一项
+# print(vec_num[1:2]) 输出一定序列范围的项
+# print(vec_char[c(1, 3)]) 输出第几项
+
+# vec_logi_gt5 <- vec_num >= 5
+# print(vec_logi_gt5)
+
+# vec_char2 <- paste(c("red", "yellow", "orange", "green"), vec_char)
+# print(vec_char2)
+# output: ## [1] "red apple" "yellow banana" "orange mandarin" "green melon"
+# Q: paste? A:形式上组合到一起，没有逻辑上的加减关系
+
+# quanteda in X-student research group
+
